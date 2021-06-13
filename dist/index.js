7941d5af48aadcad6e49f9363287c73448aa8abd
adityawankhede5 <adityawankhede555@gmail.com>
2021-06-13 17:10:53 +0530

    Inital commit.

---
+++ dist/index.js
@@ -0,0 +1,15 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+Object.defineProperty(exports, "Yourcomponent", {
+  enumerable: true,
+  get: function get() {
+    return _YourComponent.default;
+  }
+});
+
+var _YourComponent = _interopRequireDefault(require("./components/YourComponent"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
